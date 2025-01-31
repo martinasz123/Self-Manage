51bb462422c416dbc70b2b973b83ae1423045d4c
MartinSomogyi <martinasz123@gmail.com>
2025-01-31 19:48:18 +0000

    Initial Commit

---
+++ Task Manager/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace Task_Manager;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+	}
+
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		return new Window(new AppShell());
+	}
+}
---
+++ Task Manager/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Task_Manager;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
